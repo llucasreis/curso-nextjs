bc3fc0ee232ec644e302e22bb88fe5720d1a2d0f
lprs110 <pro.lucasreis@gmail.com>
2020-11-20 09:36:10 -0400

    feat: add prismic integration

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Curso NextJS
+
+## CMS Alternatives
+
+- Ghost (Blogs)
+- Strapi (Small projects)
+- Prismic (Static Pages / Landing Pages / Medium projects)
+- Contentful (Big projects)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
 import SEO from '@/components/SEO';
+import { client } from '@/lib/prismic';
 import { GetServerSideProps } from 'next';
+import Link from 'next/link';
 // import { useEffect, useState } from 'react';
 import { Title } from '../styles/pages/Home';
-
-interface IProduct {
-  id: string;
-  title: string;
-}
+import Prismic from 'prismic-javascript';
+import PrismicDOM from 'prismic-dom';
+import { Document } from 'prismic-javascript/types/documents';
 
 interface HomeProps {
-  recommendedProducts: IProduct[];
+  recommendedProducts: Document[];
 }
 
 export default function Home({ recommendedProducts }: HomeProps) {
@@ -24,12 +24,6 @@ export default function Home({ recommendedProducts }: HomeProps) {
   //   })
   // }, []);
 
-  async function handleSum() {
-    // dynamic import
-    const math = (await import('../lib/math')).default;
-    alert(math.sum(3, 5));
-  }
-
   return (
     <div>
       <SEO
@@ -41,17 +35,19 @@ export default function Home({ recommendedProducts }: HomeProps) {
         <Title>Products</Title>
 
         <ul>
-          {recommendedProducts.map(recommendedProducts => {
+          {recommendedProducts.map(recommendedProduct => {
             return (
-              <li key={recommendedProducts.id}>
-                {recommendedProducts.title}
+              <li key={recommendedProduct.id}>
+                <Link href={`/catalog/products/${recommendedProduct.uid}`}>
+                  <a>
+                    {PrismicDOM.RichText.asText(recommendedProduct.data.title)}
+                  </a>
+                </Link>
               </li>
             )
           })}
         </ul>
       </section>
-
-      <button onClick={handleSum}>Sum!</button>
     </div>
   )
 }
@@ -59,13 +55,16 @@ export default function Home({ recommendedProducts }: HomeProps) {
 // SERVER SIDE RENDERING
 // TTFB -> Time To First Byte
 export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/recommended`);
+  const recommendedProducts = await client().query([
+    Prismic.Predicates.at('document.tyoe', 'product')
+  ])
+  // const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/recommended`);
 
-  const recommendedProducts = await response.json();
+  // const recommendedProducts = await response.json();
 
   return {
     props: {
-      recommendedProducts
+      recommendedProducts: recommendedProducts.results
     }
   }
 }
